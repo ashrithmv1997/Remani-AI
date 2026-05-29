edc3469fd07e0a982bfb06a35094ba3483d3a5f5
ashrithmv1997 <5zs2c5gc4m@privaterelay.appleid.com>
2026-05-29 19:02:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function sendMessage() {
   setStatus("Thinking... 🤔");
   setAvatar("remani-default.jpg");
 
-  const res = await fetch("https://remani-api.sweatysuitcase.co.in/chat", {
+  const res = await fetch("https://remani-ai.ashrithmv.workers.dev", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: text })
